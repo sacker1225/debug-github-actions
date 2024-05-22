da0c14dc3a51ace76822cf3bcbc78cf5b3824e37
bear Li <38783339+sacker1225@users.noreply.github.com>
2024-05-22 17:16:01 +0800

    Create debug-github-actions.sh

---
+++ debug-github-actions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+if [[ -z "$NGROK_TOKEN" ]]; then
+  echo "Please set 'NGROK_TOKEN'"
+  exit 2
+fi
+
+if [[ -z "$USER_PASS" ]]; then
+  echo "Please set 'USER_PASS' for user: $USER"
+  exit 3
+fi
+
+echo "### Install ngrok ###"
+
+wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-386.tgz
+tar -xvzf ngrok-v3-stable-linux-386.tgz
+chmod +x ./ngrok
+
+echo "### Update user: $USER password ###"
+echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+
+echo "### Start ngrok proxy for 22 port ###"
+
+
+rm -f .ngrok.log
+./ngrok config add-authtoken "$NGROK_TOKEN"
+./ngrok tcp 22 --log ".ngrok.log" &
+
+sleep 10
+HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+
+if [[ -z "$HAS_ERRORS" ]]; then
+  echo ""
+  echo "=========================================="
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "=========================================="
+else
+  echo "$HAS_ERRORS"
+  exit 4
+fi
