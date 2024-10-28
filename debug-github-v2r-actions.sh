2189a1bc71afc7f6854b79e1d34d637bb7595096
bear Li <38783339+sacker1225@users.noreply.github.com>
2024-10-28 17:03:28 +0800

    Update debug-github-v2r-actions.sh

--- debug-github-v2r-actions.sh
+++ debug-github-v2r-actions.sh
@@ -6,15 +6,15 @@ chmod 755 -R v2r
 
 echo "### download v2r success ### "
 
-mkdir v2ray/config
+mkdir v2r/conf
 
-cp -f config.json v2ray/config.json
-cp -f VMess-TCP-000.json v2ray/config
+cp -f config.json v2r/config.json
+cp -f VMess-TCP-000.json v2r/conf/VMess-TCP-000.json
 
-./v2ray run -config ./v2r/config.json -confdir ./v2r/conf &
+./v2r/v2ray run -config ./v2r/config.json -confdir ./v2r/conf &
 
 sleep 10
 
 echo "### v2ray running ###"
 
-echo "vless://3c16baca-de3a-47dc-9fb8-f5fbc6b616a4@127.0.0.1:43963?encryption=none&security=reality&flow=xtls-rprx-vision&type=tcp&sni=www.ebay.com&pbk=7Vem4qMZbHtnS271U28GDP6H73lUjm_CfiEK8SLnAkM&fp=chrome#233boy-reality-127.0.0.1"
+echo "vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIjIzM2JveS1xdWljLTAwMCIsDQogICJhZGQiOiAiMTI3LjAuMC4xIiwNCiAgInBvcnQiOiAiNDc5NjMiLA0KICAiaWQiOiAiM2FjOWE0ZjYtNTI3ZS00YmViLTg3NjMtODljMThjNjBhZTI4IiwNCiAgImFpZCI6ICIwIiwNCiAgInNjeSI6ICJhdXRvIiwNCiAgIm5ldCI6ICJxdWljIiwNCiAgInR5cGUiOiAibm9uZSIsDQogICJob3N0IjogIiIsDQogICJwYXRoIjogIiIsDQogICJ0bHMiOiAiIiwNCiAgInNuaSI6ICIiLA0KICAiYWxwbiI6ICIiLA0KICAiZnAiOiAiIg0KfQ=="
