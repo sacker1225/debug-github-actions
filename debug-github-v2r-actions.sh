eb4805e860c1761f1d6d249ae5d4b6e3fe4e76fd
bear Li <38783339+sacker1225@users.noreply.github.com>
2024-10-28 16:31:50 +0800

    Update debug-github-v2r-actions.sh

--- debug-github-v2r-actions.sh
+++ debug-github-v2r-actions.sh
@@ -11,8 +11,10 @@ mkdir v2ray/config
 cp -f config.json v2ray/config.json
 cp -f VMess-TCP-000.json v2ray/config
 
-./v2ray run -config ./v2ray/config.json -confdir ./v2ray/conf &
+./v2ray run -config ./v2r/config.json -confdir ./v2r/conf &
 
 sleep 10
 
 echo "### v2ray running ###"
+
+echo "vless://3c16baca-de3a-47dc-9fb8-f5fbc6b616a4@127.0.0.1:43963?encryption=none&security=reality&flow=xtls-rprx-vision&type=tcp&sni=www.ebay.com&pbk=7Vem4qMZbHtnS271U28GDP6H73lUjm_CfiEK8SLnAkM&fp=chrome#233boy-reality-127.0.0.1"
